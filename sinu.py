8ee39c56a14408a20b753dbe3e2ed0e6b8e979a9
iserveu <iserveu@in>
2024-03-21 20:07:06 +0530

    3rd

---
+++ sinu.py
@@ -0,0 +1,20 @@
+
+# a=input("Type a number : ")
+# b=input("2nd value : ")
+# c= a<b
+# # print (type(c),c)
+# d=a>=b
+# if c is True :
+#     print('hello world')
+
+# elif d is True :
+#     print('helloworld 2')
+    
+# else:
+#     print('false')
+f= 'hello world'
+print(f)
+my_list=[1,5,6,78,46,"hello","world",]
+first_list=my_list[4:7]
+first_list.append(["raju","herry","papu","sinu"])
+print('last word : ',first_list,":",len(first_list))
